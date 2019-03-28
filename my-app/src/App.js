f7a98a41c01a8839b5a177854ee3d1ea8997ba4f
Christopher Dang <Lynda@Lyndas-MacBook-Pro.local>
2019-03-28 17:51:55 -0500

    Changed location of pages import for clarity

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import SendPackage from './Pages/SendPackage/SendPackage'
 
 //Pages
 import Home from './Pages/Home/Home';
 import Tracking from './Pages/Tracking/Tracking'
+import SendPackage from './Pages/SendPackage/SendPackage'
 
 //Components
 import Navbar from './Components/Navbar/Navbar';
