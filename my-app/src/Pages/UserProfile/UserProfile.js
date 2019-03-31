cd5c62c879e85822b0f0eb214ad45ffd79051232
asimpletouch <48941229+asimpletouch@users.noreply.github.com>
2019-03-31 02:51:18 -0500

    More user profile

--- my-app/src/Pages/UserProfile/UserProfile.js
+++ my-app/src/Pages/UserProfile/UserProfile.js
@@ -1,12 +1,75 @@
-import React, { Component } from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
 
-class UserProfile extends Component {
-    render() {
-        return (
-            <div>
-                Profile
-            </div>
-        )
-    }
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing.unit
+  }
+});
+
+function UserProfile(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+        User Profile
+      <div>
+        <FormControl className={classes.formControl} disabled>
+          <InputLabel htmlFor="component-helper">
+            <b>EMAIL</b>
+          </InputLabel>
+          <Input
+            id="email"
+            // must make value hold call database to get correct email
+            value="my email"
+            //onChange={this.handleChange}
+          />
+        </FormControl>
+      </div>
+
+      <div>
+        <FormControl className={classes.formControl} disabled>
+          <InputLabel htmlFor="component-disabled">
+            <b>USERNAME</b>
+          </InputLabel>
+          <Input
+            id="username"
+            // must make value hold call database to get correct username
+            value="my username"
+            //onChange={this.handleChange}
+          />
+        </FormControl>
+      </div>
+
+      <div>
+        <FormControl className={classes.formControl} disabled>
+          <InputLabel htmlFor="component-disabled">
+            <b>PASSWORD</b>
+          </InputLabel>
+          <Input
+            id="password"
+            // dont show password keep value ******** but allow change in password
+            value="************"
+            //onChange={this.handleChange}
+          />
+        </FormControl>
+      </div>
+
+      
+    </div>
+  );
 }
-export default UserProfile;
+
+UserProfile.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(UserProfile);
