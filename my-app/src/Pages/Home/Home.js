a64def93fa02d0857db5e2e485f6c1b6d877a3ad
jose F Boccalandro <jofebocc@gmail.com>
2019-04-08 18:30:20 -0500

    better homepage
    hello

--- my-app/src/Pages/Home/Home.js
+++ my-app/src/Pages/Home/Home.js
@@ -3,28 +3,9 @@ import { Carousel } from 'react-bootstrap';
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const styles = theme => ({
-  overrider: {
-    margin: "5px",
-  },
-  wrapper: {
-    width: "70%",
-    margin: "0 auto",
-    padding: "70px 0 0 0"
-  },
-  root: {
-    align: "center",
-    width: "100%",
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto',
-    padding: 10,
-  },
-  table: {
-    minWidth: 700,
-  },
-});
 
 export default class HomeCarousel extends Component {
   state = {
@@ -42,28 +23,22 @@ export default class HomeCarousel extends Component {
   render() {
     return (
       <div>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="picture"
-              src={"boxes4.jpg"}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h1>Tracking number</h1>
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <Button onClick={() => this.buttonConfirm()}  href = "/tracking" variant="danger">Search</Button>
-                </InputGroup.Prepend>
-                <FormControl aria-describedby="basic-addon1" Input
-                  name="Tracking_ID"
-                  value={this.state.Tracking_ID}
-                  onChange={e => this.handleChange(e.target.name, e.target.value)} />
-              </InputGroup>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-        </Carousel>
+        <Image src="boxes3.jpg" fluid />
+        <div style={{ position: "relative", bottom: 400, width: 700, margin: "0 auto" }}>
+          <h1 style={{ color: "black" , fontWeight:700 , fontSize:80 }}>Tracking number</h1>
+          <InputGroup className="mb-3" size = "lg">
+            <InputGroup.Prepend>
+              <Button onClick={() => this.buttonConfirm()} href="/tracking" variant="danger">Search</Button>
+            </InputGroup.Prepend>
+            <FormControl placeholder="Tracking ID"
+              aria-label="Tracking ID"
+              aria-describedby="basic-addon2"
+               aria-describedby="basic-addon1" Input
+              name="Tracking_ID"
+              value={this.state.Tracking_ID}
+              onChange={e => this.handleChange(e.target.name, e.target.value)} />
+          </InputGroup>
+        </div>
 
 
       </div>
