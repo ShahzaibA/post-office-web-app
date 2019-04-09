e5b21a0e031e40ea1651f9329e65b69315e5830d
Christopher Dang <dchrisu@yahoo.com>
2019-04-09 00:50:35 -0500

    Dynamic navbar in place. Resize to check.

--- my-app/src/Components/Navbar/Navbar.js
+++ my-app/src/Components/Navbar/Navbar.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from './logo.png';
 import Button from '@material-ui/core/Button';
-import EmployeeNavBarMenu from './EmployeeNavBarMenu'
+import EmployeeNavBarMenu from './EmployeeNavBarMenu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 function EmployeeNavBar(props) {
     //if (props.employee_email !== null) {
@@ -20,6 +22,30 @@ class Navbar extends Component {
     state = {
         user_ID: localStorage.getItem('sender_ID'),
         employee_email: localStorage.getItem('employee_email'),
+        width: 0,
+        height: 0,
+        anchorEl: null,
+    }
+
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget })
+    }
+    handleClose = () => {
+        this.setState({ anchorEl: null })
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = { width: 0, height: 0 };
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    componentDidMount() { this.updateWindowDimensions(); window.addEventListener('resize', this.updateWindowDimensions); }
+    componentWillUnmount() { //window.removeEventListener('resize', this.updateWindowDimensions); 
+    }
+
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight })
     }
 
     processLogout = () => {
@@ -29,35 +55,69 @@ class Navbar extends Component {
         this.setState({ employee_email: null });
     }
 
-
-
     render() {
+        const { anchorEl } = this.state;
         return (
             <header>
-                <div class="container">
-                    <a href="/"><Link to="/"><img class='logo' src={logo} alt="ShipIt" /></Link></a>
-                    <nav>
-                        <ul>
-                            <li><a href="/"><Link to="/">Home</Link></a></li>
-                            <li><a href="/send_package"><Link to="/send_package">Send Package</Link></a></li>
+                {this.state.width < 1200 ? (
+                    <div class="container" style={{ alignContent: 'center', margin: '0 auto' }}>
+                        <a href="/"><Link to="/"><img class='logo' src={logo} alt="ShipIt" /></Link></a>
+                        <Button
+                            aria-owns={anchorEl ? 'simple-menu' : undefined}
+                            aria-haspopup="true"
+                            onClick={this.handleClick}
+                        >
+                            ShipIt Menu
+                        </Button>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={anchorEl}
+                            open={Boolean(anchorEl)}
+                            onClose={this.handleClose}
+                        >
+                            <a href="/"><Link to="/"><MenuItem onClick={this.handleClose}>Home</MenuItem></Link></a>
+                            <a href="/send_package"><Link to="/send_package"><MenuItem onClick={this.handleClose}>Send Package</MenuItem></Link></a>
 
                             {this.state.user_ID === null && this.state.employee_email === null ? (
-                                <li class='LoginNav' ><a href="/login"><Link to="/login">Login</Link></a></li>
+                                <a href="/login"><Link to="/login"><MenuItem onClick={this.handleClose} class='LoginNav'>Login</MenuItem></Link></a>
                             ) : ([
-                                <li><a href="/invoice"><Link to="/invoice">Invoice</Link></a></li>,
-                                <li><a href="/user_profile"><Link to="/user_profile">Profile</Link></a></li>,
-                                <li><a href="/"><Link to="/" onClick={this.processLogout}>Logout</Link></a></li>,
+                                <a href="/invoice"><Link to="/invoice"><MenuItem onClick={this.handleClose}>Invoice</MenuItem></Link></a>,
+                                <a href="/user_profile"><Link to="/user_profile"><MenuItem onClick={this.handleClose}>Profile</MenuItem></Link></a>,
+                                <a href="/"><Link to="/" onClick={this.processLogout}><MenuItem onClick={this.handleClose} class='LoginNav'>Logout</MenuItem></Link></a>,
                             ])}
                             {this.state.employee_email !== null ? (
                                 <EmployeeNavBar employee_email={(this.state.employee_email)} />
                             ) : (
                                     <div></div>
                                 )}
+                        </Menu>
+                    </div>) :
+
+                    (<div class="container">
+                        <a href="/"><Link to="/"><img class='logo' src={logo} alt="ShipIt" /></Link></a>
+                        <nav>
+                            <ul>
+                                <li><a href="/"><Link to="/">Home</Link></a></li>
+                                <li><a href="/send_package"><Link to="/send_package">Send Package</Link></a></li>
+
+                                {this.state.user_ID === null && this.state.employee_email === null ? (
+                                    <li class='LoginNav' ><a href="/login"><Link to="/login">Login</Link></a></li>
+                                ) : ([
+                                    <li><a href="/invoice"><Link to="/invoice">Invoice</Link></a></li>,
+                                    <li><a href="/user_profile"><Link to="/user_profile">Profile</Link></a></li>,
+                                    <li><a href="/"><Link to="/" onClick={this.processLogout}>Logout</Link></a></li>,
+                                ])}
+                                {this.state.employee_email !== null ? (
+                                    <EmployeeNavBar employee_email={(this.state.employee_email)} />
+                                ) : (
+                                        <div></div>
+                                    )}
 
 
-                        </ul>
-                    </nav>
-                </div>
+                            </ul>
+                        </nav>
+                    </div>)
+                }
             </header >
         );
     }
