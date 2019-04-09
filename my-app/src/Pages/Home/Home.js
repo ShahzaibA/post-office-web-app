e9976bb7a8d789b4d83e97f12bad67d5cfa519a9
Christopher Dang <dchrisu@yahoo.com>
2019-04-09 01:56:03 -0500

    Responsive web design on home.js

--- my-app/src/Pages/Home/Home.js
+++ my-app/src/Pages/Home/Home.js
@@ -21,27 +21,25 @@ export default class HomeCarousel extends Component {
 
   render() {
     return (
-      <div>
-        <Image src="boxes3.jpg" fluid />
-        <div style={{ position: "relative", bottom: 400, width: 700, margin: "0 auto" }}>
-          <h1 style={{ color: "black" , fontWeight:700 , fontSize:80 }}>Tracking number</h1>
-          <InputGroup className="mb-3" size = "lg">
+      <div class='container' flex style={{ margin: "0 auto", position: 'absolute', top: '65px', left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Image src="boxes3.jpg" fluid style={{ margin: "0 auto", position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center' }} />
+        <div flex style={{ position: "relative", padding: "20px", margin: "0 auto", width: "85%", top: '10%' }}>
+          <h1 fluid style={{ color: "black", fontWeight: 700, fontSize: '3.5vw' }}>Tracking number</h1>
+          <InputGroup className="mb-3" size="lg">
             <InputGroup.Prepend>
-              <Button onClick={() => this.buttonConfirm()} href="/tracking" variant="danger">Search</Button>
+              <Button onClick={() => this.buttonConfirm()} href="/tracking" variant="danger" style={{ fontSize: '1.5vw', }}>Search</Button>
             </InputGroup.Prepend>
             <FormControl placeholder="Tracking ID"
+              style={{ fontSize: '1.5vw' }}
               aria-label="Tracking ID"
               aria-describedby="basic-addon2"
-               aria-describedby="basic-addon1" Input
+              aria-describedby="basic-addon1" Input
               name="Tracking_ID"
               value={this.state.Tracking_ID}
               onChange={e => this.handleChange(e.target.name, e.target.value)} />
           </InputGroup>
         </div>
-
-
-
-      </div>
+      </div >
     );
   }
 }
