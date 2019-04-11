829e099bf2a6a550e727e74082900744d39c69d9
Shahzaib Ali <shahzaibsa98@gmail.com>
2019-04-11 01:08:54 -0500

    Created delivery frontend for drivers

---
+++ my-app/src/Pages/Delivered/Delivered.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Button } from 'react-bootstrap';
+
+const styles = theme => ({
+    root: {
+        width: '90%',
+        maxHeight: 500,
+        marginTop: 0,
+        margin: 'auto',
+        overflowX: 'auto',
+        overflowY: 'auto'
+    },
+    table: {
+        minWidth: 700,
+    },
+    header: {
+        position: 'sticky',
+        top: 0,
+        backgroundColor: '#F9F4F3',
+        color: 'black'
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 150,
+    },
+    button: {
+        marginTop: theme.spacing.unit * 3,
+        marginLeft: theme.spacing.unit,
+    },
+});
+
+class Delivered extends Component {
+
+    state = {
+        packages: [],
+    }
+
+    componentDidMount() {
+        this.getPackagesToDeliver();
+    }
+
+    handleChange = (name, val) => {
+        this.setState({ [name]: val });
+    };
+
+    getPackagesToDeliver = () => {
+        fetch('http://localhost:4000/get_deliveries', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                Employee_Email: "shahzaibsa98@gmail.com",
+            })
+        })
+            .then(res => res.json())
+            .then(result => this.setState({ packages: result.data }))
+            .catch(err => console.log(err))
+    }
+
+    arrivalScan(Package_ID, Hub_ID) {
+        fetch('http://localhost:4000/arrival_scan', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                Package_ID: Package_ID,
+                Hub_ID: Hub_ID,
+            })
+        })
+            .then(() => this.getPackagesAwaitingArrival())
+            .catch(err => console.log(err))
+    }
+
+    renderPackageList = ({ Package_ID, Shipping_Address, Shipping_City, Shipping_State_Abbr, Shipping_Zip, Hub_ID }) =>
+        <TableRow key={Package_ID}>
+            <TableCell component="th" scope="row">{Package_ID}</TableCell>
+            <TableCell align="right">{Shipping_Address}</TableCell>
+            <TableCell align="right">{Shipping_City}</TableCell>
+            <TableCell align="right">{Shipping_State_Abbr}</TableCell>
+            <TableCell align="right">{Shipping_Zip}</TableCell>
+            <TableCell align="right"><Button size='sm' variant="outline-danger" onClick={() => this.arrivalScan(Package_ID, Hub_ID)} >Delivered</Button></TableCell>
+        </TableRow>
+
+    render() {
+        const { classes } = this.props;
+        const { packages } = this.state;
+        console.log(packages);
+        return (
+            <React.Fragment>
+                <h1>Deliveries</h1>
+                <Paper className={classes.root}>
+                    <Table className={classes.table}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell className={classes.header}>Package #</TableCell>
+                                <TableCell align="right" className={classes.header}>Shipping Address</TableCell>
+                                <TableCell align="right" className={classes.header}>Shipping City</TableCell>
+                                <TableCell align="right" className={classes.header}>Shipping State</TableCell>
+                                <TableCell align="right" className={classes.header}>Shipping Zip</TableCell>
+                                <TableCell align="right" className={classes.header}>Mark Delivered</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody >
+                            {packages.map(this.renderPackageList)}
+                        </TableBody>
+                    </Table>
+                </Paper>
+            </React.Fragment>
+        );
+    }
+}
+
+Delivered.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Delivered);
